428993f9001e6bff53c474588a3ee9cbea972fe6
Szymon Dziwisz <szymon.dziwisz1@gmail.com>
2019-11-05 16:44:51 +0100

    Updates

---
+++ precache-manifest.c9619e5a1f16f55bfdf4af70bf8b2dae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7f041df92e5d245de06409b0bae3db1",
+    "url": "/user-list-krk/index.html"
+  },
+  {
+    "revision": "d43e16a56886aa9f287c",
+    "url": "/user-list-krk/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "f987ba54d7f8a7512792",
+    "url": "/user-list-krk/static/js/2.6edbfc48.chunk.js"
+  },
+  {
+    "revision": "d43e16a56886aa9f287c",
+    "url": "/user-list-krk/static/js/main.cc1c7a6a.chunk.js"
+  },
+  {
+    "revision": "3e7bdb3f912a3d1e6c5d",
+    "url": "/user-list-krk/static/js/runtime-main.680eedba.js"
+  }
+]);
